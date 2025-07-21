870a309e845af54922d8e952d393b0e616fa4e5e
rachit-developer24 <srachit841@gmail.com>
2025-07-22 00:55:54 +0100

    Initial Commit

---
+++ AirBnbProjectTests/AirBnbProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbProjectTests.swift
+//  AirBnbProjectTests
+//
+//  Created by Rachit Sharma on 22/07/2025.
+//
+
+import Testing
+@testable import AirBnbProject
+
+struct AirBnbProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
